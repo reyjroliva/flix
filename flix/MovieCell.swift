395abfc5fb47337f15de425a8c6dfafc5ebfa796
Rey Oliva <reyjroliva@MacBook-Pro-203.local>
2017-06-23 15:27:34 -0700

    Finished required user stories and changed background to black and text to white

--- flix/MovieCell.swift
+++ flix/MovieCell.swift
@@ -15,6 +15,7 @@ class MovieCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     
+    @IBOutlet weak var posterImageView: UIImageView!
     
     
     override func awakeFromNib() {
--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -7,21 +7,38 @@
 //
 
 import UIKit
-
+import AlamofireImage
 private let reuseIdentifier = "Cell"
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource{
-
+    
     @IBOutlet weak var tableView: UITableView!
+   
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     
     var movies: [[String: Any]] = []
-    
+    var refreshControl: UIRefreshControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
+        fetchMovies()
         
+    }
+    
+    func didPullToRefresh(_ refreshControl: UIRefreshControl){
         
+        fetchMovies()
+    }
+    
+    
+    func fetchMovies(){
+        activityIndicator.startAnimating()
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -35,12 +52,14 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource{
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
+        activityIndicator.stopAnimating()
         task.resume()
         
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
@@ -54,12 +73,26 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource{
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterURL)
+        
         
         return cell
     }
     
     
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell){
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+            
+        }
+    }
     
     
     
