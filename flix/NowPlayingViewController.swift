b6845898ee29d79618e8159f7cb20dc4cd76bdfb
Rey Oliva <reyjroliva@MacBook-Pro-203.local>
2017-06-23 15:44:13 -0700

    Fixed selection effect on movie view controller

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -90,7 +90,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource{
             let movie = movies[indexPath.row]
             let detailViewController = segue.destination as! DetailViewController
             detailViewController.movie = movie
-            
+            tableView.deselectRow(at: indexPath, animated: true)
         }
     }
     
