8f8ba2c75e22b921c03b4f27c1a05671d970c8b7
Rey Oliva <reyjroliva@MacBook-Pro-203.local>
2017-06-23 17:20:52 -0700

    Added basic collection view

---
+++ flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Rey Oliva on 6/23/17.
+//  Copyright © 2017 Rey Oliva. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageVIew: UIImageView!
+}
